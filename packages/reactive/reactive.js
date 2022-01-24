ed7b1488a34af89c2f82b886bf12dc3e87339601
LAPTOP-RI2VH723\LVO <15827969980@163.com>
2022-01-24 16:55:58 +0800

    修改computed

--- packages/reactive/reactive.js
+++ packages/reactive/reactive.js
@@ -1,10 +1,14 @@
-
-const targetMap = new WeakMap() 
+const targetMap = new WeakMap()
 let activeEffect = null
-export function effect(eff){
-    activeEffect = eff  
-    activeEffect()
+export function effect(eff, option = {}) {
+    if (option.lazy) {
+        eff.scheduler = option.scheduler
+        option.scheduler()
+        activeEffect = option.scheduler
 
+    }
+    activeEffect = eff
+    activeEffect()
     activeEffect = null
 }
 
@@ -14,74 +18,115 @@ export function effect(eff){
 //console.log(`total:${total};salePrice:${salePrice.value}`) //13.5,4.5
 
 //console.log(`total:${total};salePrice:${salePrice.value}`) //27,9 
-function track(target,key){      
+function track(target, key) {
 
-    if(activeEffect){
+    if (activeEffect) {
         let depsMap = targetMap.get(target)
-        if(!depsMap){
+        if (!depsMap) {
             depsMap = new Map()
-            targetMap.set(target,depsMap)
+            targetMap.set(target, depsMap)
         }
         let dep = depsMap.get(key)
-        if(!dep){
+        if (!dep) {
             dep = new Set()
-            depsMap.set(key,dep)
+            depsMap.set(key, dep)
         }
         dep.add(activeEffect)
-        
+
     }
-   
+
 }
-function trigger(target,key){
+
+function trigger(target, key) {
     let depsMap = targetMap.get(target)
-    if(!depsMap){ return }
+    if (!depsMap) {
+        return
+    }
     let dep = depsMap.get(key)
-    if(dep){
-        
-        dep.forEach(effect => effect())
+    if (dep) {
+
+        dep.forEach(effect => {
+            if (effect.scheduler) {
+                
+                effect.scheduler()
+            } else {
+                effect()
+            }
+
+        })
     }
 }
-export function reactive(target){
+export function reactive(target) {
     const handler = {
-        get(target,key,reciver){          
-            let result = Reflect.get(target,key,reciver)
-           
-            track(target,key)
+        get(target, key, reciver) {
+            let result = Reflect.get(target, key, reciver)
+
+            track(target, key)
             return result
         },
-        set(target,key,value,reciver){   
-            if(target[key] !== value){
-                var result = Reflect.set(target,key,value,reciver) //boolean
-                trigger(target,key)
+        set(target, key, value, reciver) {
+            if (target[key] !== value) {
+                var result = Reflect.set(target, key, value, reciver) //boolean
+                trigger(target, key)
             }
             return result
         }
     }
-    return new Proxy(target,handler)
+    return new Proxy(target, handler)
 }
 
-export function ref(raw){
+export function ref(raw) {
     const r = {
-        get value(){
-            track(r,'value')
+        get value() {
+            track(r, 'value')
             return raw
         },
-        set value(newValue){
-            if(raw !== newValue){
+        set value(newValue) {
+            if (raw !== newValue) {
                 raw = newValue
-                trigger(r,'value')
+                trigger(r, 'value')
             }
         }
     }
     return r
 }
 
-export function computed(getter){
-    let result = ref()
-    effect(() => {
-        result.value = getter()
-    })
-    return result
+export function computed(getter) {
+    /*     let result = ref()
+        effect(() => {
+            result.value = getter()
+        }) */
+    return new ReactiveComputed(getter)
 }
 
 
+class ReactiveComputed {
+    constructor(getter) {
+        this.dirty = true
+        this._value = null
+        this.getter = getter
+        this.effect = effect(getter, {
+            lazy: true,
+            scheduler: () => {
+                console.log('scheduler')
+                if (!this.dirty) {
+                    this.dirty = true
+                    //trigger(this,'value')                    
+                }
+
+            }
+        })
+    }
+    get value() {
+        console.log(this.dirty)
+        if (this.dirty) {
+            this._value = this.getter()
+            this.dirty = false
+            //track(this,'value')
+        }
+        return this._value
+    }
+    set value(newValue) {
+        this._value = newValue
+    }
+}
--- src/components/AppChild.js
+++ src/components/AppChild.js
@@ -3,24 +3,34 @@ import { ref, reactive, computed } from '../../packages/reactive/reactive'
 export default {    
     setup(){
         const num = ref(0)
+        window.n = num
+        
         function changeNum(){
             num.value++
             num.value++
             num.value++
             num.value++
         }
+        const c = computed(() => {
+            console.log('ccc')
+            return num.value * 2
+        })
+        window.c = c
         return {
             num,
-            changeNum
+            changeNum,
+            c
         }
     },
     updated(){
         console.log('updated')
     },
     render(proxy){
+        console.log('c')
         return h('div',{class:'colorGreen'},[
             h('p',{class:'appChild'},'I am appChild----' + proxy.num.value),
-            h('button',{onClick:proxy.changeNum},'changeNumBtn')
+            h('button',{onClick:proxy.changeNum},'changeNumBtn'),
+            h('h2',{},`${proxy.c.value}`)
         ])
     }
 }
